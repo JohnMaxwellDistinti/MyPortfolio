1d1a4490664334d534886f4f84d6c4917989ec45
John Maxwell Distinti <JohnMaxwellDistinti@gmail.com>
2023-09-07 00:49:52 -0700

    Fixed mobile action detection

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -15,28 +15,67 @@ import purplesky from "./img/cyberpunk.png";
 const WEBHOOK_URL =
   "https://discord.com/api/webhooks/1149217975711309914/2xlddn_mktlx-xhS0oGfxHBPL6Q_l4wOBNY7e-ySRY58do5ypzshuKvTM6mHzgEfK3Qe";
 
+const isTouchScreenDevice = () => {
+  try {
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
 function postToWebhook(ip, msg, color) {
-  let embeds = [
-    {
-      title: ip + " - " + msg,
-      color: color,
-    },
-  ];
-  var config = {
-    method: "POST",
-    url: WEBHOOK_URL, // https://discord.com/webhook/url/here
-    headers: { "Content-Type": "application/json" },
-    data: JSON.stringify({ embeds }),
-  };
-  axios(config)
-    .then((response) => {
-      // console.log("Webhook delivered successfully");
-      return response;
-    })
-    .catch((error) => {
-      // console.log(error);
-      return error;
-    });
+  var isMobile = isTouchScreenDevice();
+  if (isMobile === false) {
+    let embeds = [
+      {
+        title: ip + " - " + msg,
+        color: color,
+      },
+    ];
+    var config = {
+      method: "POST",
+      url: WEBHOOK_URL, // https://discord.com/webhook/url/here
+      headers: { "Content-Type": "application/json" },
+      data: JSON.stringify({ embeds }),
+    };
+    axios(config)
+      .then((response) => {
+        // console.log("Webhook delivered successfully");
+        return response;
+      })
+      .catch((error) => {
+        // console.log(error);
+        return error;
+      });
+  }
+}
+
+function postToWebhookMobile(ip, msg, color) {
+  var isMobile = isTouchScreenDevice();
+  if (isMobile) {
+    let embeds = [
+      {
+        title: ip + "(MOBILE) - " + msg,
+        color: color,
+      },
+    ];
+    var config = {
+      method: "POST",
+      url: WEBHOOK_URL, // https://discord.com/webhook/url/here
+      headers: { "Content-Type": "application/json" },
+      data: JSON.stringify({ embeds }),
+    };
+    axios(config)
+      .then((response) => {
+        // console.log("Webhook delivered successfully");
+        return response;
+      })
+      .catch((error) => {
+        // console.log(error);
+        return error;
+      });
+  }
 }
 
 const Portfolio = () => {
@@ -45,10 +84,16 @@ const Portfolio = () => {
   const getData = async () => {
     const res = await axios.get("https://api.ipify.org/?format=json");
     setIP(res.data.ip);
-    postToWebhook(res.data.ip, "New Visitor!", 5174599);
+    var isMobile = isTouchScreenDevice();
+    if (isMobile) {
+      postToWebhookMobile(res.data.ip, "New Visitor!", 5174599);
+    } else {
+      postToWebhook(res.data.ip, "New Visitor!", 5174599);
+    }
   };
 
   const shouldLog = useRef(true);
+  const didTouch = useRef(true);
   useEffect(() => {
     if (shouldLog.current) {
       shouldLog.current = false;
@@ -71,6 +116,9 @@ const Portfolio = () => {
           onClick={() =>
             postToWebhook(ip, "Clicked LinkedIn Profile!", 15548997)
           }
+          onTouchStart={() =>
+            postToWebhookMobile(ip, "Clicked LinkedIn Profile!", 15548997)
+          }
         >
           <img src={linkedin} alt="LinkedIn" className="icon" />
         </a>
@@ -79,18 +127,27 @@ const Portfolio = () => {
           onClick={() =>
             postToWebhook(ip, "Clicked Twitter(X) Profile!", 15548997)
           }
+          onTouchStart={() =>
+            postToWebhookMobile(ip, "Clicked Twitter(X) Profile!", 15548997)
+          }
         >
           <img src={x} alt="X" className="icon" />
         </a>
         <a
           href="https://github.com/JohnMaxwellDistinti"
           onClick={() => postToWebhook(ip, "Clicked GitHub Profile!", 15548997)}
+          onTouchStart={() =>
+            postToWebhookMobile(ip, "Clicked GitHub Profile!", 15548997)
+          }
         >
           <img src={github} alt="GitHub" className="icon" />
         </a>
         <a
           href="https://www.roblox.com/users/16012710/profile"
           onClick={() => postToWebhook(ip, "Clicked Roblox Profile!", 15548997)}
+          onTouchStart={() =>
+            postToWebhookMobile(ip, "Clicked Roblox Profile!", 15548997)
+          }
         >
           <img src={roblox} alt="Roblox" className="icon" />
         </a>
